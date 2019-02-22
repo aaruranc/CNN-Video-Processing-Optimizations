5c54686cbf1d858f812176d5bccae13891c5b480
Aaruran Chandrasekhar <aaruranc@umich.edu>
2019-02-22 12:39:32 -0500

    updated detection file

--- Test/basic_detection.py
+++ Test/basic_detection.py
@@ -9,7 +9,7 @@ import zipfile
 from distutils.version import StrictVersion
 from collections import defaultdict
 from io import StringIO
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 from PIL import Image
 from object_detection.utils import ops as utils_ops
 
@@ -121,17 +121,17 @@ if __name__ == '__main__':
 	  # Actual detection.
 	  output_dict = run_inference_for_single_image(image_np, detection_graph)
 	  # Visualization of the results of a detection.
-	  # vis_util.visualize_boxes_and_labels_on_image_array(
-	  #     image_np,
-	  #     output_dict['detection_boxes'],
-	  #     output_dict['detection_classes'],
-	  #     output_dict['detection_scores'],
-	  #     category_index,
-	  #     instance_masks=output_dict.get('detection_masks'),
-	  #     use_normalized_coordinates=True,
-	  #     line_thickness=8)
-	  # plt.figure(figsize=IMAGE_SIZE)
-	  # plt.imshow(image_np)
+	  vis_util.visualize_boxes_and_labels_on_image_array(
+	      image_np,
+	      output_dict['detection_boxes'],
+	      output_dict['detection_classes'],
+	      output_dict['detection_scores'],
+	      category_index,
+	      instance_masks=output_dict.get('detection_masks'),
+	      use_normalized_coordinates=True,
+	      line_thickness=8)
+	  plt.figure(figsize=IMAGE_SIZE)
+	  plt.imshow(image_np)
 	  box_ctr = 0;
 	  box_count = output_dict['num_detections']
 	  print(output_dict)
